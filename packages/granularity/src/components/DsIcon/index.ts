9f18bcce51d93fe3af9e73e7dfbafe4021e6d5fa
fureev <fureev@gmail.com>
2026-04-22 19:02:38 +0300

    ### chore: refactor DsIcon component

--- packages/granularity/src/components/DsIcon/index.ts
+++ packages/granularity/src/components/DsIcon/index.ts
@@ -1,6 +1,3 @@
 export {default} from './DsIcon.vue'
 export {default as DsIcon} from './DsIcon.vue'
 export {dsIconConfig} from './config'
-
-export type {DsIconSize} from './dsIconStyles'
-export {resolveDsIconSizePx} from './dsIconStyles'
